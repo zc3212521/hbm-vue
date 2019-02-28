48189804b46e659b9066b25b6201925f05fc2ea6
zhangchao <13075311322@189.cn>
2019-02-28 18:52:57 +0800

    add antd

---
+++ src/components/common/HMenu/HSubMenu/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <span v-if="menuData.length">
+    <a-menu-item v-for="(item, index) in menuData" :key="index">
+      <template v-if="item.children && item.children.length">
+        <a-sub-menu>
+          <a-icon :type="item.icon" />
+          <span>{{item.name}}</span>
+          <h-sub-menu :menuData="item.children"></h-sub-menu>
+        </a-sub-menu>
+      </template>
+      <template v-else>
+        <a-icon :type="item.icon" />
+        <span>{{item.name}}</span>
+      </template>
+    </a-menu-item>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'HSubMenu',
+  props: {
+    menuData: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/components/common/HMenu/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <a-menu theme="dark" mode="inline" :defaultSelectedKeys="['1']">
+    <h-sub-ment :menuData="menuData"></h-sub-ment>
+
+    <!--<template v-for="(item, index) in menuData">-->
+      <!--<template v-if="item.children && item.children.length > 0">-->
+        <!--<a-sub-menu :key="index">-->
+          <!--<span slot="title">-->
+            <!--<a-icon :type="item.icon" />-->
+            <!--<span>{{item.name}}</span>-->
+          <!--</span>-->
+          <!--<h-sub-ment-->
+            <!--v-for="(item0, index0) in item.children"-->
+            <!--:key="index0"-->
+            <!--:menuData="item0">-->
+          <!--</h-sub-ment>-->
+        <!--</a-sub-menu>-->
+      <!--</template>-->
+      <!--<template v-else>-->
+        <!--<a-menu-item :key="index">-->
+          <!--<a-icon :type="item.icon" />-->
+          <!--<span>{{item.name}}</span>-->
+        <!--</a-menu-item>-->
+      <!--</template>-->
+    <!--</template>-->
+  </a-menu>
+</template>
+
+<script>
+// import HSubMenu from './HSubMenu'
+
+export default {
+  name: 'hMenu',
+  components: {
+    // HSubMenu
+  },
+  data () {
+    return {
+      menuData: [
+        {
+          name: 'page1',
+          path: '/page1',
+          icon: 'mail',
+          children: [
+            {
+              name: 'sub1',
+              path: '/home/sub1',
+              icon: 'mail',
+              children: [
+                {
+                  name: 'sub1-1',
+                  path: '/home/sub1',
+                  icon: 'mail'
+                },
+                {
+                  name: 'sub1-2',
+                  path: '/home/sub2',
+                  icon: 'mail'
+                }
+              ]
+            },
+            {
+              name: 'sub1',
+              path: '/home/sub1',
+              icon: 'mail'
+            },
+            {
+              name: 'sub1',
+              path: '/home/sub1',
+              icon: 'mail'
+            }
+          ]
+        },
+        {
+          name: 'page2',
+          path: '/page2',
+          icon: 'mail'
+        },
+        {
+          name: 'page3',
+          path: '/page3',
+          icon: 'mail'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
