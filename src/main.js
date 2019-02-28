f2de18ad56eb5bcde04701257e6bcac6a47146e0
zhangchao <13075311322@189.cn>
2019-02-28 15:10:03 +0800

    add antd

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './plugins/ant-design-vue.js'
+import './base/antd-vue-config.js'
 
 process.env.NODE_ENV === 'mock' && require('./mock/index.js')
 
